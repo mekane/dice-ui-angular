d02cfa3b8737ab899fbb78cff7a3c95d1b5f02f2
Marty Kane <marty@pixotech.com>
2016-08-12 15:28:15 -0500

    Add roll button and generate stats

--- src/components/diceRoller.component.js
+++ src/components/diceRoller.component.js
@@ -1,13 +1,31 @@
 angular.module('diceapp')
     .component('diceRoller', {
-        bindings: {},
         controller: function () {
-            console.log('Dice Roller init');
+            var ctrl = this;
 
+            ctrl.diceConfig = [
+                {number: 1, size: 4},
+                {number: 1, size: 6},
+                {number: 1, size: 8},
+                {number: 1, size: 10},
+                {number: 1, size: 12}
+            ];
+
+            ctrl.modifier = 0;
+
+            ctrl.roll = function () {
+                var diceToRoll = window.dice.convertDiceToListOfDiceSizes(ctrl.diceConfig);
+                var rolls = window.dice.computeRollsForDice(diceToRoll);
+                var stats = window.dice.getPercentageStatsFromTotals(window.dice.combineTotals(rolls));
+                console.log('Stats', stats);
+            }
         },
         template: [
             '<form name="diceForm">',
-            '  <dice-input number="1" size="6">',
+            '  <dice-input ng-repeat="die in $ctrl.diceConfig" number="die.number" size="die.size"></dice-input>',
+            '  <span class="dice-form__plus">+</span>',
+            '  <input class="dice-form__modifier" type="text" ng-model="$ctrl.modifier">',
+            '  <button class="dice-form__button dice-form__roll" type="button" ng-click="$ctrl.roll()">Roll!</button>',
             '</form>'
         ].join('')
     });
